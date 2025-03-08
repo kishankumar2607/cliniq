27fafd57e2b4c2764db5e52a63dc06e7e0fd34d0
Kishan Kumar Das <kishankumar2672001@gmail.com>
2025-03-08 14:18:23 -0500

    Update encryptDecryptData.js

--- src/app/components/encryptDecryptData.js
+++ src/app/components/encryptDecryptData.js
@@ -1,28 +1,25 @@
+
 import { AES, enc } from "crypto-js";
 
 const SECRET_KEY = process.env.NEXT_PUBLIC_SECRET_KEY;
 
-// Function to encrypt data
+// Encrypt data
 export const encryptData = (value) => {
   const encryptedValue = AES.encrypt(value, SECRET_KEY).toString();
   return encryptedValue;
 };
 
-// Function to decrypt data
+// Decrypt data
 export const decryptData = (encryptedValue) => {
   try {
-    if (!encryptedValue) {
+    if (!encryptedValue || typeof encryptedValue !== "string") {
       return "";
     }
     const bytes = AES.decrypt(encryptedValue, SECRET_KEY);
-
-    if (!bytes) {
-      return "";
-    }
     const decryptedData = bytes.toString(enc.Utf8);
-    return decryptedData || null;
+    return decryptedData || "";
   } catch (error) {
     console.error(`Error decrypting data`, error);
-    return null;
+    return "";
   }
 };
